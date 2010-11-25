15d70ca1b5b8188142150eefc396ffb0285a1572
selvan <p.thamarai@gmail.com>
2010-11-25 18:08:49 +0530

    Support to limit items to be fetched for wildcard & starts_with methods. Trie spec updated accordingly

--- spec/trie_spec.js
+++ spec/trie_spec.js
@@ -41,31 +41,89 @@ describe("non-empty trie", function() {
     });
 
     it("should match wildcards", function () {
-        expect(_trie.wildcard("H*ll.")).toEqual([
-            { key : 'Hilly', value : 'World' },
-            { key : 'Hello', value : 'World' }
-        ]);
-        expect(_trie.wildcard("Hel")).toEqual([])
-    });
 
-    it("should match starts_with", function() {
-        expect(_trie.starts_with("xyz")).toEqual([]);
-        expect(_trie.starts_with("He")).toEqual([
+        /* Fetch all */
+        expect(_trie.wildcard("H*ll.", -1)).toEqual([
             { key : 'Hello', value : 'World' },
-            { key : 'Hello, bob', value : 'World' },
-            { key : 'Hello, brother', value : 'World' }
-        ]);
-
-        expect(_trie.starts_with("Hi")).toEqual([
             { key : 'Hilly', value : 'World' }
         ]);
 
-        expect(_trie.starts_with("H")).toEqual([
-            { key : 'Hilly', value : 'World' },
+        /* Fetch 2 */
+        expect(_trie.wildcard("H*ll.", 2)).toEqual([
+            { key : 'Hello', value : 'World' },
+            { key : 'Hilly', value : 'World' }
+        ]);
+
+        expect(_trie.wildcard("H*ll.", 1)).toEqual([
+            { key : 'Hello', value : 'World' }
+        ]);
+
+        expect(_trie.wildcard("H*ll.", 0)).toEqual([]);
+
+        expect(_trie.wildcard("Hel", 10)).toEqual([])
+    });
+
+    it("should match starts_with", function() {
+        expect(_trie.starts_with("xyz", 1)).toEqual([]);
+
+        /* Fetch all */
+        expect(_trie.starts_with("H", -1)).toEqual([
+            { key : 'Hello', value : 'World' },
+            { key : 'Hello, bob', value : 'World' },
+            { key : 'Hello, brother', value : 'World' },
+            { key : 'Hilly', value : 'World' }
+        ]);
+
+        /* Fetch 4 */
+        expect(_trie.starts_with("H", 4)).toEqual([
+            { key : 'Hello', value : 'World' },
+            { key : 'Hello, bob', value : 'World' },
+            { key : 'Hello, brother', value : 'World' },
+            { key : 'Hilly', value : 'World' }
+        ]);
+
+        expect(_trie.starts_with("H", 1)).toEqual([
+            { key : 'Hello', value : 'World' }
+        ]);
+
+        expect(_trie.starts_with("H", 0)).toEqual([]);
+
+        expect(_trie.starts_with("He", 2)).toEqual([
+            { key : 'Hello', value : 'World' },
+            { key : 'Hello, bob', value : 'World' }
+        ]);
+
+        expect(_trie.starts_with("He", 3)).toEqual([
             { key : 'Hello', value : 'World' },
             { key : 'Hello, bob', value : 'World' },
             { key : 'Hello, brother', value : 'World' }
         ]);
+
+        expect(_trie.starts_with("Hi", 1)).toEqual([
+            { key : 'Hilly', value : 'World' }
+        ]);
+
+        expect(_trie.starts_with("Hi", 100)).toEqual([
+            { key : 'Hilly', value : 'World' }
+        ]);
+    });
+
+    it("should fetch all values", function () {
+
+        /* Fetch all */
+        expect(_trie.all(-1)).toEqual([
+            { key : 'Hello', value : 'World' },
+            { key : 'Hello, bob', value : 'World' },
+            { key : 'Hello, brother', value : 'World' },
+            { key : 'Hilly', value : 'World' }
+        ]);
+
+        expect(_trie.all(2)).toEqual([
+            { key : 'Hello', value : 'World' },
+            { key : 'Hello, bob', value : 'World' }
+        ]);
+
+        expect(_trie.all(0)).toEqual([]);        
     });
 });
 
